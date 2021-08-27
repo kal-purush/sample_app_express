52575edb7d0a2733833ab570a628a638a6a7dd97
Masudul Hasan Masud <masudulhasanmasud690@gmail.com>
2021-08-27 22:27:46 +0200

    add cluster in express

--- app.js
+++ app.js
@@ -1,72 +1,87 @@
-const express = require('express')
-const {performance} = require('perf_hooks');
-const app = express()
+// const throng = require('throng')
 
-function insertionSort(inputArr) {
-    let n = inputArr.length;
-        for (let i = 1; i < n; i++) {
-            // Choosing the first element in our unsorted subarray
-            let current = inputArr[i];
-            // The last element of our sorted subarray
-            let j = i-1;
-            while ((j > -1) && (current < inputArr[j])) {
-                inputArr[j+1] = inputArr[j];
-                j--;
-            }
-            inputArr[j+1] = current;
-        }
-    return inputArr;
-}
+const WORKERS = process.env.WEB_CONCURRENCY || 2
 
-// app.get('/', function (req, res) {
-//     // console.log(req);
-//     // var user_id = req.params['id']
-//     // console.log(req.query.id)
+const PORT = process.env.PORT || 8080
 //
-//     var v1 = performance.now();
-//     id = req.query.id
-//     if (id == null){
-//         // console.log("There is no id");
-//     }else{
-//         var str = '';
-//         for(var i=0; i<id; i++){
-//             str += "a";
-//         }
-//         str += "c";
-//         // console.log(str);
-//         var myRe =/^((a)*)+b$/;
-//         var myArray = myRe.test(str);
-//         // console.log(myArray);
-//     }
-//     var v2 = performance.now();
-//     // console.log("total time  taken = "+(v2-v1)+"milliseconds");
-//     // res.send('Hello World');
-//     res.send("total time  taken = "+(v2-v1)+" milliseconds")
-// })
+// throng(WORKERS, start)
+//
+// function start(workid) {
+//   const express = require('express')
+//   const {performance} = require('perf_hooks');
+//   const app = express()
+//
+//   function rsa_computation(limit) {
+//       let p = 178542003245811211274167228297361192303886321036074276889145691522634525820185614278499562592134188995169731066418203258297035264969457638591284906658912408319763156912951486020761069099132619194489006875108217247513715271974383296142805846405783845170862140174184507256128825312324419293575432423822703857091;
+//       let r = 187;
+//           for (let i = 1; i < limit; i++) {
+//             p=(p*p)%r;
+//           }
+//       return p;
+//   }
+//
+//   app.get('/', function (req, res) {
+//       var v1 = performance.now();
+//       id = req.query.id
+//       if (id == null){
+//       }else{
+//           rsa_computation(id);
+//       }
+//       var v2 = performance.now();
+//       res.send("total time  taken = "+(v2-v1)+" milliseconds")
+//   })
+//   app.listen(PORT);
+//   // console.log('Listening on', PORT)
+// }
 
-function rsa_computation(limit) {
-    let p = 178542003245811211274167228297361192303886321036074276889145691522634525820185614278499562592134188995169731066418203258297035264969457638591284906658912408319763156912951486020761069099132619194489006875108217247513715271974383296142805846405783845170862140174184507256128825312324419293575432423822703857091;
-    let r = 187;
-        for (let i = 1; i < limit; i++) {
-          p=(p*p)%r;
-        }
-    return p;
-}
 
-app.get('/', function (req, res) {
-    var v1 = performance.now();
-    id = req.query.id
-    if (id == null){
-        // console.log("There is no id");
-    }else{
-        rsa_computation(id);
+var cluster = require('cluster');
+
+if(cluster.isMaster) {
+    var numWorkers = WORKERS;
+
+    console.log('Master cluster setting up ' + numWorkers + ' workers...');
+
+    for(var i = 0; i < numWorkers; i++) {
+        cluster.fork();
     }
-    var v2 = performance.now();
-    // console.log("total time  taken = "+(v2-v1)+"milliseconds");
-    // res.send('Hello World');
-    res.send("total time  taken = "+(v2-v1)+" milliseconds")
-})
 
+    cluster.on('online', function(worker) {
+        console.log('Worker ' + worker.process.pid + ' is online');
+    });
 
-var port = process.env.PORT || 3000;
-app.listen(port);
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+        console.log('Starting a new worker');
+        cluster.fork();
+    });
+} else {
+    const express = require('express')
+    const {performance} = require('perf_hooks');
+    const app = express()
+
+    function rsa_computation(limit) {
+        let p = 178542003245811211274167228297361192303886321036074276889145691522634525820185614278499562592134188995169731066418203258297035264969457638591284906658912408319763156912951486020761069099132619194489006875108217247513715271974383296142805846405783845170862140174184507256128825312324419293575432423822703857091;
+        let r = 187;
+            for (let i = 1; i < limit; i++) {
+              p=(p*p)%r;
+            }
+        return p;
+    }
+
+    app.get('/', function (req, res) {
+        var v1 = performance.now();
+        id = req.query.id
+        if (id == null){
+        }else{
+            rsa_computation(id);
+        }
+        var v2 = performance.now();
+        res.send("total time  taken = "+(v2-v1)+" milliseconds")
+    })
+    // app.listen(PORT);
+
+    var server = app.listen(PORT, function() {
+        console.log('Process ' + process.pid + ' is listening to all incoming requests');
+    });
+}
